31c2d732596ea43b4b02db1483f59e2dfa259324
Biswadeep Ray <biswa7102raydeep@gmail.com>
2023-11-19 23:38:25 +0530

    bug fix

--- src/shared/constants/api.constant.ts
+++ src/shared/constants/api.constant.ts
@@ -1,4 +1,4 @@
-// export const BASE_URL = "https://localhost:7101/api"
+export const BASE_URL = "https://localhost:7101/api"
 //export const BASE_URL = "http://13.51.13.96:5000/api"
 
-export const BASE_URL = "https://2h25uhsoa1.execute-api.eu-north-1.amazonaws.com/api"
+//export const BASE_URL = "https://2h25uhsoa1.execute-api.eu-north-1.amazonaws.com/api"
